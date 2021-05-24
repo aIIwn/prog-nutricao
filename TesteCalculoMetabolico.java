22c15c5b1b7fc70cf8c875374e2167dca9f56354
Wender <60801175+aIIwn@users.noreply.github.com>
2021-05-24 12:00:30 -0300

    Finalizado os testes.

--- TesteCalculoMetabolico.java
+++ TesteCalculoMetabolico.java
@@ -1,7 +1,6 @@
 package entidades;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,17 +14,24 @@ class TesteCalculoMetabolico {
 	}
 
 	@Test
-	void testTaxaMetabolica() {		
+	void testTaxaMetabolica() {
 		assertEquals("CASO 01: ", 2191.8, calculoMetabolico.taxaMetabolica(27, 75.0, 'M', 1), 0);
-		assertEquals("CASO 01: ", 1918.2, calculoMetabolico.taxaMetabolica(27, 75.0, 'F', 1), 0);		
-		//terminar os caso para entrar nas faixas etarias.
+		assertEquals("CASO 02: ", 1918.2, calculoMetabolico.taxaMetabolica(27, 75.0, 'F', 1), 0);
+		assertEquals("CASO 03: ", 2576.4, calculoMetabolico.taxaMetabolica(27, 75.9, 'M', 2), 0);
+		assertEquals("CASO 04: ", 2095.2, calculoMetabolico.taxaMetabolica(27, 75.9, 'F', 2), 0);
+		assertEquals("CASO 05: ", 2392.5, calculoMetabolico.taxaMetabolica(45, 82.3, 'M', 3), 0);
+		assertEquals("CASO 06: ", 2240.3, calculoMetabolico.taxaMetabolica(45, 82.3, 'F', 3), 0);
+		assertEquals("CASO 07: ", 2581.6, calculoMetabolico.taxaMetabolica(15, 55.0, 'M', 4), 0);
+		assertEquals("CASO 08: ", 2125.5, calculoMetabolico.taxaMetabolica(15, 55.0, 'F', 4), 0);
+		assertEquals("CASO 09: ", 2711.2, calculoMetabolico.taxaMetabolica(62, 75.5, 'M', 5), 0);
+		assertEquals("CASO 10: ", 2360.9, calculoMetabolico.taxaMetabolica(62, 75.5, 'F', 5), 0);
 	}
 
 	@Test
 	void testFaixaEtaria() {
-		assertEquals("CASO 21: ", 0, calculoMetabolico.faixaEtaria(10), 0);
-		assertEquals("CASO 22: ", 1, calculoMetabolico.faixaEtaria(21), 0);
-		assertEquals("CASO 23: ", 2, calculoMetabolico.faixaEtaria(60), 0);
-		assertEquals("CASO 24: ", 3, calculoMetabolico.faixaEtaria(62), 0);
+		assertEquals("CASO 11: ", 0, calculoMetabolico.faixaEtaria(10), 0);
+		assertEquals("CASO 12: ", 1, calculoMetabolico.faixaEtaria(21), 0);
+		assertEquals("CASO 13: ", 2, calculoMetabolico.faixaEtaria(60), 0);
+		assertEquals("CASO 14: ", 3, calculoMetabolico.faixaEtaria(62), 0);
 	}
 }
