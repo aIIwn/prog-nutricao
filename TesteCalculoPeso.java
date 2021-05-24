05d934e5d7fed2e4bec5325fba5358b0d9d57e2d
Wender <60801175+aIIwn@users.noreply.github.com>
2021-05-24 11:59:52 -0300

    Ajustado e finalizado os testes.

--- TesteCalculoPeso.java
+++ TesteCalculoPeso.java
@@ -24,41 +24,43 @@ class TesteCalculoPeso {
 	void testCondicaoIMC() { //ARRUMAR OS NUMEROS.
 		assertEquals("CASO 04: ", "Abaixo do Peso", calculoPeso.condicaoIMC(19.3));
 		assertEquals("CASO 05: ", "Peso Normal", calculoPeso.condicaoIMC(20.1));
-		assertEquals("CASO 05: ", "Peso Normal", calculoPeso.condicaoIMC(24.9));
-		assertEquals("CASO 05: ", "Peso Normal", calculoPeso.condicaoIMC(25));
-		assertEquals("CASO 05: ", "Sobre Peso", calculoPeso.condicaoIMC(25.1));
-		assertEquals("CASO 07: ", "Sobre Peso", calculoPeso.condicaoIMC(30));
-		assertEquals("CASO 08: ", "Obeso", calculoPeso.condicaoIMC(30.1));
-		assertEquals("CASO 08: ", "Obeso", calculoPeso.condicaoIMC(40));
-		assertEquals("CASO 09: ", "Obeso Mórbido", calculoPeso.condicaoIMC(40.1));
-		assertEquals("CASO 09: ", "Obeso Mórbido", calculoPeso.condicaoIMC(45));
+		assertEquals("CASO 06: ", "Peso Normal", calculoPeso.condicaoIMC(24.9));
+		assertEquals("CASO 07: ", "Peso Normal", calculoPeso.condicaoIMC(25));
+		assertEquals("CASO 08: ", "Sobre Peso", calculoPeso.condicaoIMC(25.1));
+		assertEquals("CASO 09: ", "Sobre Peso", calculoPeso.condicaoIMC(30));
+		assertEquals("CASO 10: ", "Obeso", calculoPeso.condicaoIMC(30.1));
+		assertEquals("CASO 11: ", "Obeso", calculoPeso.condicaoIMC(40));
+		assertEquals("CASO 12: ", "Obeso MÃ³rbido", calculoPeso.condicaoIMC(40.1));
+		assertEquals("CASO 13: ", "Obeso MÃ³rbido", calculoPeso.condicaoIMC(45));
 	}
-
+	
 	@Test
 	void testPesoIdeal() {
-		assertEquals("CASO 07: ", 65.8, calculoPeso.pesoIdeal(27, 1.73, 'M'), 0);
-		assertEquals("CASO 08: ", 62.9, calculoPeso.pesoIdeal(27, 1.73, 'F'), 0);
-		assertEquals("CASO 09: ", 53.5, calculoPeso.pesoIdeal(30, 1.56, 'M'), 0);
-		assertEquals("CASO 10: ", 51.1, calculoPeso.pesoIdeal(30, 1.56, 'F'), 0);
-		assertEquals("CASO 11: ", 72.7, calculoPeso.pesoIdeal(65, 1.73, 'M'), 0);
-		assertEquals("CASO 12: ", 79.3, calculoPeso.pesoIdeal(69, 1.73, 'F'), 0);
-		assertEquals("CASO 13: ", 85.9, calculoPeso.pesoIdeal(70, 1.85, 'M'), 0);
-		assertEquals("CASO 14: ", 90.0, calculoPeso.pesoIdeal(74, 1.85, 'F'), 0);
-		assertEquals("CASO 15: ", 69.1, calculoPeso.pesoIdeal(75, 1.70, 'M'), 0);
-		assertEquals("CASO 16: ", 75.4, calculoPeso.pesoIdeal(79, 1.70, 'F'), 0);		
-		assertEquals("CASO 17: ", 66.1, calculoPeso.pesoIdeal(80, 1.67, 'M'), 0);
-		assertEquals("CASO 18: ", 71.1, calculoPeso.pesoIdeal(84, 1.67, 'F'), 0);		
-		assertEquals("CASO 19: ", 67.5, calculoPeso.pesoIdeal(85, 1.71, 'M'), 0);
-		assertEquals("CASO 20: ", 69.0, calculoPeso.pesoIdeal(89, 1.71, 'F'), 0);
+		assertEquals("CASO 14: ", 23.0, calculoPeso.pesoIdeal(7, 1.10, 'M'), 0);
+		assertEquals("CASO 15: ", 29.0, calculoPeso.pesoIdeal(10, 1.37, 'F'), 0);
+		assertEquals("CASO 16: ", 65.8, calculoPeso.pesoIdeal(27, 1.73, 'M'), 0);
+		assertEquals("CASO 17: ", 62.9, calculoPeso.pesoIdeal(27, 1.73, 'F'), 0);
+		assertEquals("CASO 18: ", 53.5, calculoPeso.pesoIdeal(30, 1.56, 'M'), 0);
+		assertEquals("CASO 19: ", 51.1, calculoPeso.pesoIdeal(30, 1.56, 'F'), 0);
+		assertEquals("CASO 20: ", 72.7, calculoPeso.pesoIdeal(65, 1.73, 'M'), 0);
+		assertEquals("CASO 21: ", 79.3, calculoPeso.pesoIdeal(69, 1.73, 'F'), 0);
+		assertEquals("CASO 22: ", 85.9, calculoPeso.pesoIdeal(70, 1.85, 'M'), 0);
+		assertEquals("CASO 23: ", 90.0, calculoPeso.pesoIdeal(74, 1.85, 'F'), 0);
+		assertEquals("CASO 24: ", 69.1, calculoPeso.pesoIdeal(75, 1.70, 'M'), 0);
+		assertEquals("CASO 25: ", 75.4, calculoPeso.pesoIdeal(79, 1.70, 'F'), 0);		
+		assertEquals("CASO 26: ", 66.1, calculoPeso.pesoIdeal(80, 1.67, 'M'), 0);
+		assertEquals("CASO 27: ", 71.1, calculoPeso.pesoIdeal(84, 1.67, 'F'), 0);		
+		assertEquals("CASO 28: ", 67.5, calculoPeso.pesoIdeal(85, 1.71, 'M'), 0);
+		assertEquals("CASO 29: ", 69.0, calculoPeso.pesoIdeal(89, 1.71, 'F'), 0);
 	}
 
 	@Test
 	void testImcFaixaEtaria() {
-		assertEquals("CASO 21: ", 0, calculoPeso.imcFaixaEtaria(18), 0);
-		assertEquals("CASO 22: ", 1, calculoPeso.imcFaixaEtaria(65), 0);
-		assertEquals("CASO 23: ", 2, calculoPeso.imcFaixaEtaria(70), 0);
-		assertEquals("CASO 24: ", 3, calculoPeso.imcFaixaEtaria(75), 0);
-		assertEquals("CASO 25: ", 4, calculoPeso.imcFaixaEtaria(80), 0);
-		assertEquals("CASO 26: ", 5, calculoPeso.imcFaixaEtaria(85), 0);
+		assertEquals("CASO 30: ", 0, calculoPeso.imcFaixaEtaria(18), 0);
+		assertEquals("CASO 31: ", 1, calculoPeso.imcFaixaEtaria(65), 0);
+		assertEquals("CASO 32: ", 2, calculoPeso.imcFaixaEtaria(70), 0);
+		assertEquals("CASO 33: ", 4, calculoPeso.imcFaixaEtaria(80), 0);
+		assertEquals("CASO 34: ", 3, calculoPeso.imcFaixaEtaria(75), 0);
+		assertEquals("CASO 35: ", 5, calculoPeso.imcFaixaEtaria(85), 0);
 	}
 }
