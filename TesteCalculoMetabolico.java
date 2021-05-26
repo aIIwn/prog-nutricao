c1f2d21656da840f6df9418a35a4f0fc839a2d93
Wender <60801175+aIIwn@users.noreply.github.com>
2021-05-26 16:46:59 -0300

    Revisado.

--- TesteCalculoMetabolico.java
+++ TesteCalculoMetabolico.java
@@ -15,16 +15,16 @@ class TesteCalculoMetabolico {
 
 	@Test
 	void testTaxaMetabolica() {
-		assertEquals("CASO 01: ", 2191.8, calculoMetabolico.taxaMetabolica(27, 75.0, "Masculino", 1), 0);
-		/*assertEquals("CASO 02: ", 1918.2, calculoMetabolico.taxaMetabolica(27, 75.0, 'F', 1), 0);
-		assertEquals("CASO 03: ", 2576.4, calculoMetabolico.taxaMetabolica(27, 75.9, 'M', 2), 0);
-		assertEquals("CASO 04: ", 2095.2, calculoMetabolico.taxaMetabolica(27, 75.9, 'F', 2), 0);
-		assertEquals("CASO 05: ", 2392.5, calculoMetabolico.taxaMetabolica(45, 82.3, 'M', 3), 0);
-		assertEquals("CASO 06: ", 2240.3, calculoMetabolico.taxaMetabolica(45, 82.3, 'F', 3), 0);
-		assertEquals("CASO 07: ", 2581.6, calculoMetabolico.taxaMetabolica(15, 55.0, 'M', 4), 0);
-		assertEquals("CASO 08: ", 2125.5, calculoMetabolico.taxaMetabolica(15, 55.0, 'F', 4), 0);
-		assertEquals("CASO 09: ", 2711.2, calculoMetabolico.taxaMetabolica(62, 75.5, 'M', 5), 0);
-		assertEquals("CASO 10: ", 2360.9, calculoMetabolico.taxaMetabolica(62, 75.5, 'F', 5), 0);*/
+		assertEquals("CASO 01: ", 2191.8, calculoMetabolico.taxaMetabolica(27, 75.0, "Masculino", 0), 0);
+		assertEquals("CASO 02: ", 1918.2, calculoMetabolico.taxaMetabolica(27, 75.0, "Feminino", 0), 0);
+		assertEquals("CASO 03: ", 2576.4, calculoMetabolico.taxaMetabolica(27, 75.9, "Masculino", 1), 0);
+		assertEquals("CASO 04: ", 2095.2, calculoMetabolico.taxaMetabolica(27, 75.9, "Feminino", 1), 0);
+		assertEquals("CASO 05: ", 2392.5, calculoMetabolico.taxaMetabolica(45, 82.3, "Masculino", 2), 0);
+		assertEquals("CASO 06: ", 2240.3, calculoMetabolico.taxaMetabolica(45, 82.3, "Feminino", 2), 0);
+		assertEquals("CASO 07: ", 2581.6, calculoMetabolico.taxaMetabolica(15, 55.0, "Masculino", 3), 0);
+		assertEquals("CASO 08: ", 2125.5, calculoMetabolico.taxaMetabolica(15, 55.0, "Feminino", 3), 0);
+		assertEquals("CASO 09: ", 2711.2, calculoMetabolico.taxaMetabolica(62, 75.5, "Masculino", 4), 0);
+		assertEquals("CASO 10: ", 2360.9, calculoMetabolico.taxaMetabolica(62, 75.5, "Feminino", 4), 0);
 	}
 
 	@Test
