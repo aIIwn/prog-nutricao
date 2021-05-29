29731100effebd8ab9aab60988f3d471b868400b
Wender <60801175+aIIwn@users.noreply.github.com>
2021-05-29 13:29:07 -0300

    Library correction and refactored.

--- TesteCalculoMetabolico.java
+++ TesteCalculoMetabolico.java
@@ -1,37 +1,35 @@
-package entidades;
+package testes;
 
-import static org.junit.Assert.assertEquals;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
-class TesteCalculoMetabolico {
+import entidades.CalculoMetabolico;
+
+public class TesteCalculoMetabolico {
 	
-	calculoMetabolico teste;	
-
-	@BeforeEach
-	void setUp() throws Exception {
-		teste = new calculoMetabolico();
+	String sexoMasculino = "Masculino";
+	String sexoFeminino = "Feminino";
+	
+	@Test
+	public void testTaxaMetabolica() {
+		assertEquals("CASO 01: ", 2191.8, CalculoMetabolico.taxaMetabolica(27, 75.0, sexoMasculino, 0), 0);
+		assertEquals("CASO 02: ", 1918.2, CalculoMetabolico.taxaMetabolica(27, 75.0, sexoFeminino, 0), 0);
+		assertEquals("CASO 03: ", 2576.4, CalculoMetabolico.taxaMetabolica(27, 75.9, sexoMasculino, 1), 0);
+		assertEquals("CASO 04: ", 2095.2, CalculoMetabolico.taxaMetabolica(27, 75.9, sexoFeminino, 1), 0);
+		assertEquals("CASO 05: ", 2392.5, CalculoMetabolico.taxaMetabolica(45, 82.3, sexoMasculino, 2), 0);
+		assertEquals("CASO 06: ", 2240.3, CalculoMetabolico.taxaMetabolica(45, 82.3, sexoFeminino, 2), 0);
+		assertEquals("CASO 07: ", 2581.6, CalculoMetabolico.taxaMetabolica(15, 55.0, sexoMasculino, 3), 0);
+		assertEquals("CASO 08: ", 2125.5, CalculoMetabolico.taxaMetabolica(15, 55.0, sexoFeminino, 3), 0);
+		assertEquals("CASO 09: ", 2711.2, CalculoMetabolico.taxaMetabolica(62, 75.5, sexoMasculino, 4), 0);
+		assertEquals("CASO 10: ", 2360.9, CalculoMetabolico.taxaMetabolica(62, 75.5, sexoFeminino, 4), 0);
 	}
 
 	@Test
-	void testTaxaMetabolica() {
-		assertEquals("CASO 01: ", 2191.8, calculoMetabolico.taxaMetabolica(27, 75.0, "Masculino", 0), 0);
-		assertEquals("CASO 02: ", 1918.2, calculoMetabolico.taxaMetabolica(27, 75.0, "Feminino", 0), 0);
-		assertEquals("CASO 03: ", 2576.4, calculoMetabolico.taxaMetabolica(27, 75.9, "Masculino", 1), 0);
-		assertEquals("CASO 04: ", 2095.2, calculoMetabolico.taxaMetabolica(27, 75.9, "Feminino", 1), 0);
-		assertEquals("CASO 05: ", 2392.5, calculoMetabolico.taxaMetabolica(45, 82.3, "Masculino", 2), 0);
-		assertEquals("CASO 06: ", 2240.3, calculoMetabolico.taxaMetabolica(45, 82.3, "Feminino", 2), 0);
-		assertEquals("CASO 07: ", 2581.6, calculoMetabolico.taxaMetabolica(15, 55.0, "Masculino", 3), 0);
-		assertEquals("CASO 08: ", 2125.5, calculoMetabolico.taxaMetabolica(15, 55.0, "Feminino", 3), 0);
-		assertEquals("CASO 09: ", 2711.2, calculoMetabolico.taxaMetabolica(62, 75.5, "Masculino", 4), 0);
-		assertEquals("CASO 10: ", 2360.9, calculoMetabolico.taxaMetabolica(62, 75.5, "Feminino", 4), 0);
+	public void testFaixaEtaria() {
+		assertEquals("CASO 11: ", 0, CalculoMetabolico.faixaEtaria(10), 0);
+		assertEquals("CASO 12: ", 1, CalculoMetabolico.faixaEtaria(21), 0);
+		assertEquals("CASO 13: ", 2, CalculoMetabolico.faixaEtaria(60), 0);
+		assertEquals("CASO 14: ", 3, CalculoMetabolico.faixaEtaria(62), 0);
 	}
 
-	@Test
-	void testFaixaEtaria() {
-		assertEquals("CASO 11: ", 0, calculoMetabolico.faixaEtaria(10), 0);
-		assertEquals("CASO 12: ", 1, calculoMetabolico.faixaEtaria(21), 0);
-		assertEquals("CASO 13: ", 2, calculoMetabolico.faixaEtaria(60), 0);
-		assertEquals("CASO 14: ", 3, calculoMetabolico.faixaEtaria(62), 0);
-	}
 }
