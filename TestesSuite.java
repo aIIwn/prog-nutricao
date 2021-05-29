0f90fd481df944a096a689c9124f5946a6aa84a0
Wender <60801175+aIIwn@users.noreply.github.com>
2021-05-29 13:20:35 -0300

    Update Suíte de Testes.

--- TestesSuite.java
+++ TestesSuite.java
@@ -1,12 +1,15 @@
-package entidades;
+package testes;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-@RunWith (Suite.class)
-@SuiteClasses({TesteCalculoPeso.class, TesteCalculoMetabolico.class})
+@RunWith(Suite.class)
+@SuiteClasses({
+	TesteCalculoPeso.class,
+	TesteCalculoMetabolico.class,
+})
 
-public class TestesSuite {
-  
+public class SuiteTestes {
+	
 }
